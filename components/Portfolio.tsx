0021f992df951ef28129dd7f32f547e040abe058
Memo Chou <memochou1993@gmail.com>
2022-11-19 12:27:03 +0800

    Add social links

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import Image from 'next/image';
 import Card from '@/components/Card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import {
+  faGithub,
+  faFacebook,
+  faLinkedin,
+  faInstagram,
+  faTelegram,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 export default function Portfolio() {
   return (
@@ -22,9 +29,24 @@ export default function Portfolio() {
             </span>
           </div>
           <div className="flex justify-center mb-8">
-            <FontAwesomeIcon icon={faGithub} width="24" color="white" className="mx-2" />
-            <FontAwesomeIcon icon={faFacebook} width="24" color="white" className="mx-2" />
-            <FontAwesomeIcon icon={faLinkedin} width="24" color="white" className="mx-2" />
+            <a href="https://github.com/memochou1993" target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faGithub} width="24" color="white" className="mx-2" />
+            </a>
+            <a href="https://www.linkedin.com/in/memochou1993/" target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faFacebook} width="24" color="white" className="mx-2" />
+            </a>
+            <a href="https://linkedin.com/in/memochou1993" target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} width="24" color="white" className="mx-2" />
+            </a>
+            <a href="https://www.instagram.com/memochou1993/" target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faInstagram} width="24" color="white" className="mx-2" />
+            </a>
+            <a href="https://telegram.me/memochou1993" target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faTelegram} width="24" color="white" className="mx-2" />
+            </a>
+            <a href="https://twitter.com/memochou1993" target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faTwitter} width="24" color="white" className="mx-2" />
+            </a>
           </div>
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 md:gap-6 lg:gap-8">
             <Card />
