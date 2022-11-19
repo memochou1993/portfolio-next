4e81ff7ef8206d0990e04af237d335236f4706c4
Memo Chou <memochou1993@gmail.com>
2022-11-19 16:50:21 +0800

    Add tags

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,29 +6,38 @@ interface Props {
   title: string;
   image: string;
   link: string;
+  tags: Array<string>;
 }
 
 export default function Card({
   title,
   image,
   link,
+  tags,
 }: Props) {
   return (
-    <div className="max-w-sm bg-white border rounded-lg shadow-md dark:bg-slate-800 dark:border-slate-300">
+    <div className="max-w-sm bg-white border rounded-lg shadow-md shadow-slate-600/50 dark:bg-slate-800 dark:border-slate-400">
       <picture>
         <source srcSet={image} type="image/webp" />
         <img alt={title} src={image} className="rounded-t-lg aspect-auto aspect-[3/2]" />
       </picture>
-      <div className="px-4 py-8">
-        <a href={link} target="_blank" rel="noreferrer">
-          <h5 className="mb-4 text-2xl font-bold tracking-tight dark:text-slate-200">
+      <div className="px-4 py-6">
+        <div className="mb-4 text-2xl font-bold tracking-tight dark:text-slate-200">
+          <a href={link} target="_blank" rel="noreferrer">
             {title}
-          </h5>
-        </a>
-        <p className="mb-4 font-normal tracking-tight dark:text-slate-300">
-          #Solidity
+          </a>
+        </div>
+        <p className="mb-4 font-thin dark:text-slate-300">
+          {
+            tags.map((tag, i) => (
+              <span>
+                {`#${tag}`}
+                {`${i < tags.length - 1 ? ' ' : ''}`}
+              </span>
+            ))
+          }
         </p>
-        <a href={link} target="_blank" rel="noreferrer" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white rounded-lg focus:ring-1 bg-gradient-to-br from-blue-700 to-blue-600 hover:from-blue-800 hover:to-blue-700 active:from-blue-900 active:to-blue-800">
+        <a href={link} target="_blank" rel="noreferrer" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white rounded-lg ring-slate-400 hover:ring-1 active:ring-2 bg-gradient-to-br from-indigo-700 to-indigo-600 hover:from-indigo-800 hover:to-indigo-700 active:from-indigo-900 active:to-indigo-800">
           <span className="mr-2">
             Read more
           </span>
--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -6,12 +6,18 @@ export default function Portfolio() {
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 md:gap-6 lg:gap-8">
       {
-        projects.map(({ title, image, link }) => (
+        projects.map(({
+          title,
+          image,
+          link,
+          tags,
+        }) => (
           <Card
             key={title}
             title={title}
             image={image}
             link={link}
+            tags={tags}
           />
         ))
       }
