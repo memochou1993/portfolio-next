d06ac9d78c779201389862eb5257bb27144290c2
Memo Chou <memochou1993@gmail.com>
2022-11-21 01:52:28 +0800

    Add load more button

--- components/ArticleList.tsx
+++ components/ArticleList.tsx
@@ -3,6 +3,8 @@ import React, {
   useRef,
   useState,
 } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 interface ArticleProps {
   title: string;
@@ -28,9 +30,12 @@ class Article {
   }
 }
 
+const LIMIT = 25;
+
 export default function ArticleList() {
   const mounted = useRef(false);
   const [articles, setArticles] = useState<Array<Article>>([]);
+  const [page, setPage] = useState(1);
   const fetchArticles = async () => {
     const res = await fetch('https://blog.epoch.tw/atom.xml') as Response;
     const doc = new DOMParser().parseFromString(await res.text(), 'text/xml');
@@ -49,9 +54,9 @@ export default function ArticleList() {
     mounted.current = true;
   }, []);
   return (
-    <div className="w-full px-8 py-4 border divide-y divide-slate-500 rounded-xl border-slate-500 bg-slate-200 dark:bg-slate-800">
+    <div className="w-full px-8 py-2 border divide-y divide-slate-500 rounded-xl border-slate-500 bg-slate-200 dark:bg-slate-800">
       {
-        articles.map(({ title, link, published }) => {
+        articles.slice(0, LIMIT * page).map(({ title, link, published }) => {
           const [year, month, day] = (new Date(published)).toLocaleDateString().split('/');
           return (
             <div key={title} className="py-4 text-slate-800 dark:text-slate-200">
@@ -66,6 +71,18 @@ export default function ArticleList() {
             </div>
           );
         })
+      }
+      {
+        page < articles.length / LIMIT && (
+          <div className="flex justify-center pt-4 pb-2">
+            <button type="button" onClick={() => setPage(page + 1)} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white rounded-xl ring-slate-400 hover:ring-1 bg-gradient-to-br from-indigo-700 to-indigo-500 hover:from-indigo-800 hover:to-indigo-500 active:from-indigo-900 active:to-indigo-500">
+              <span className="mr-2">
+                Load more
+              </span>
+              <FontAwesomeIcon icon={faArrowDown} width="12" className="dark:text-slate-300" />
+            </button>
+          </div>
+        )
       }
     </div>
   );
